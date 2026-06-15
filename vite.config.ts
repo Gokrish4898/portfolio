34dbac0aed69c17d5a7f5d1e4f405d6cfd98d3ac
Gokrish4898 <agokul4898@gmail.com>
2026-06-16 00:07:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base:"/portfolio/",
+    base:"/portfolio",
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
